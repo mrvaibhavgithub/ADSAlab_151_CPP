50d040b96b68519d4f021927363ea34bf0a2ecb7
vaibhav nitin khandekar <vaibhavnitink@gmail.com>
2025-07-26 13:25:15 +0530

    Update assignment2.cpp

--- assignment2.cpp
+++ assignment2.cpp
@@ -8,13 +8,7 @@ same.
 Input :  arr[] = {1, 2, 0, 4, 3, 0, 5, 0}; 
 Output : arr[] = {1, 2, 4, 3, 5, 0, 0, 0}; 
 
-3. Implement a problem of smallest number with at 
-least n trailing zeroes in factorial. 
-Statement: Given a number n. The task is to find the 
-smallest number whose factorial contains at least n 
-trailing zeroes. 
-Input : n = 1                  Output : 5  
-Input : n = 6                  Output : 25
+
 */
 #include <iostream>
 using namespace std;
